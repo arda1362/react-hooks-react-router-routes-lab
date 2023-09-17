35244cebc398501f0854730d81ab6dc0395acce9
Ardavan Zareirad <ardavan2525@gmail.com>
2023-09-17 19:47:05 -0400

    complete lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { actors } from "../data";
+import React from 'react';
+import { actors } from '../data';
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>Name: {actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, i) => (
+              <li key={i}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
